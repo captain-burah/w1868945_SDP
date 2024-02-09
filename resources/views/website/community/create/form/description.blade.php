383edd3ea5513bc914bf585e1d27f37ec25c873d
cptburah <cptburah@gmail.com>
2024-02-09 15:33:16 +0400

    community editor

--- resources/views/website/community/create/form/description.blade.php
+++ resources/views/website/community/create/form/description.blade.php
@@ -16,3 +16,23 @@
         </div>
     </div>
 </div>
+
+
+
+
+<div class="row mt-4">
+
+    <div class="col-md-12">
+        <div class="mb-3">
+            <label for="basicpill-meta_title-input">Description (AR)</label>
+            <div class="w-100 ">
+                <div class="my-4">
+                    @error('description_ar')
+                        <div class="text-danger text-xs" >{{ $message }}</div>
+                    @enderror
+                    <textarea name="description_ar" id="description_ar" class="w-100">{{ old('description_ar') }}</textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
